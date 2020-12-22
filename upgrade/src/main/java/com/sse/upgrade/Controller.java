28b970b16b68a79ba6beeda58f8858d7685549c0
JonathanArns <jonathan.arns@googlemail.com>
2020-12-22 11:50:05 +0100

    add controller class

---
+++ upgrade/src/main/java/com/sse/upgrade/Controller.java
@@ -0,0 +1,13 @@
+package com.sse.upgrade;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
