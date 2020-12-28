4cf0ddac74c5c9e18d55c167abde2cbcd28dcc99
zehra <za2023602@gmail.com>
2020-12-28 18:59:38 +0100

    static js und css hinzugefügtKlasse Pruefung erstellt html template studentPruefungen erstellt, hat noch bugs

--- upgrade/src/main/java/com/sse/upgrade/Controller.java
+++ upgrade/src/main/java/com/sse/upgrade/Controller.java
@@ -1,5 +1,6 @@
 package com.sse.upgrade;
 
+import model.Pruefung;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,5 +23,16 @@ public class Controller {
             mav.addObject("studentArray", studentArray);
             return mav;
         }
+
+
+    @GetMapping("/studentAngemPruefungen")
+    public ModelAndView studentAngemPruefungen() {
+        ModelAndView mav = new ModelAndView("studentPruefungen");
+        ArrayList<Pruefung> angemPruefungen = new ArrayList<>();
+        model.Pruefung pr1 = new Pruefung();
+        angemPruefungen.add(pr1);
+        mav.addObject("angemPruefungen", angemPruefungen);
+        return mav;
+    }
 
 }
---
+++ upgrade/src/main/resources/static/js/actions.js
@@ -0,0 +1,3 @@
+function abmeldbar(){
+return true;
+}
